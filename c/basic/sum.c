f671b902384db6710505d33032e691d336a3a3d2
onlitstechnologies <onlitstechnologies@gmail.com>
2022-04-22 11:08:04 +0530

    20220422

--- c/basic/sum.c
+++ c/basic/sum.c
@@ -1,3 +1,12 @@
-scanf("%d%d",&a, &b);
+#include <stdio.h>
+int main()
+{
+int a, b, c;
+printf("Enter the first integer: ");
+scanf("%d",&a);
+printf("Enter the second integer: ");
+scanf("%d", &b);
 c = a + b;
-printf("%d", c);
+printf("%d is the sum of above integers.\n", c);
+return 0;
+}
