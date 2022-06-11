f78ea2cf9385e320f99a5147fd327b73984bdd54
onlitstechnologies <onlitstechnologies@gmail.com>
2022-06-11 15:52:33 +0530

    20220611

--- c/book-exercise/let-us-c/ch-3/b-e.c
+++ c/book-exercise/let-us-c/ch-3/b-e.c
@@ -8,9 +8,9 @@ int main()
         s=0;
         while(t>0)
         {
-            d = t/10;
-            s = s + (d*d*d);
-            t = t%10;
+            d = t%10;
+            s += (d*d*d);
+            t /= 10;
         }
         if(i == s)
         {
