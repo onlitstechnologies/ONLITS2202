4017d6bac725244873da409473651c59a280274e
onlitstechnologies <onlitstechnologies@gmail.com>
2022-06-17 15:56:25 +0530

    20220617

--- c/book-exercise/let-us-c/ch-3/b-e.c
+++ c/book-exercise/let-us-c/ch-3/b-e.c
@@ -2,17 +2,17 @@
 int main()
 {
     int i, t, d, s;
-    for(i=1; i<=500; i++)
+    for(i=1; i<=500; i++)   //outer loop
     {
         t=i;
         s=0;
-        while(t>0)
+        while(t>0)          //inner loop
         {
-            d = t%10;
-            s += (d*d*d);
-            t /= 10;
+            d = t%10;       //Extracting digit
+            s += (d*d*d);   //Calculating the sum of cubes of digits
+            t /= 10;        //Preparing/Reducing the number for next iteration
         }
-        if(i == s)
+        if(i == s)          //comparing original number with the sum of the cubes of its digits
         {
             printf("%d\n", i);
         }
