a59ab530080387a8f32434591bef98178e9e04b6
onlitstechnologies <onlitstechnologies@gmail.com>
2022-07-13 15:59:13 +0530

    20220713

--- c/function/prime-numbers.c
+++ c/function/prime-numbers.c
@@ -22,10 +22,10 @@ int main()
 int factors(int n)      //function header
 //------function body------
 {
-    int i, count=0;
+    int i, count=0;         //local variable declaration
     for(i = 1; i<=n; i++)
     {
-        if(n % i == 0)
+        if(n % i == 0)      //function statements
         {
             count++;
         }
